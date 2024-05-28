492b442d9a6b73e0c9053d23692914fa7114177d
차규진 <74246381+dadadacole@users.noreply.github.com>
2024-05-28 19:31:39 +0900

    Add files via upload

--- App.vue
+++ App.vue
@@ -1,18 +1,50 @@
 <template>
     <div id="app" class="container">
-        <inputTodo />
-        <TodoList />
+        <div class="card card-body bg-light">
+            <div class="title">Todolist app</div>
+        </div>
+        <div class="card card-default card-borderless">
+            <div class="card-body">
+                <InputTodo />
+                <TodoList />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import TodoList from 'vue-project\src\components\TodoList.vue';
-import InputTodo from 'vue-project\src\components\InputTodo.vue';
+import TodoList from './components/TodoList.vue';
+import InputTodo from './components/InputTodo.vue';
+
+let ts = new Date().getTime();
 
 export default {
     name: 'App',
-
-    //IMPORT와 짝궁 component로 등록해줘야함
     components: { InputTodo, TodoList },
+    data() {
+        return {
+            todoList: [
+                { id: ts, todo: '자전거 타기', completed: false },
+                { id: ts + 1, todo: '자전거 타기', completed: true },
+                { id: ts + 2, todo: '자전거 타기', completed: false },
+                { id: ts + 3, todo: '자전거 타기', completed: false },
+            ],
+        };
+    },
+    methods: {
+        addTodo(todo) {
+            if (todo.length >= 2) {
+                this.todoList.push({ id: new Date().getTime(), todo: todo, completed: false });
+            }
+        },
+        deleteTodo(id) {
+            let index = this.todoList.findIndex((item) => id === item.id);
+            this.todoList.splice(index, 1);
+        },
+        toggleCompleted(id) {
+            let index = this.todoList.findIndex((item) => id === item.id);
+            this.todoList[index].completed = !this.todoList[index].completed;
+        },
+    },
 };
 </script>
