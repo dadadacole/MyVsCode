5d37a82e0d912e53fbf47dbfe7f112afd78d55cc
차규진 <74246381+dadadacole@users.noreply.github.com>
2024-05-28 19:30:29 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app" class="container">
+        <inputTodo />
+        <TodoList />
+    </div>
+</template>
+
+<script>
+import TodoList from 'vue-project\src\components\TodoList.vue';
+import InputTodo from 'vue-project\src\components\InputTodo.vue';
+
+export default {
+    name: 'App',
+
+    //IMPORT와 짝궁 component로 등록해줘야함
+    components: { InputTodo, TodoList },
+};
+</script>
